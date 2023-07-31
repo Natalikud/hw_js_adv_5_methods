dad8d958256c4e95ee87de6f67e4686de6236343
Natalikud2 <Reimova091@mail.ru>
2023-07-31 20:29:33 +0800

    Commit 2. Version 2

--- src/js/character.js
+++ src/js/character.js
@@ -1,13 +1,15 @@
 export default class Character {
   constructor(name, type) {
     this.name = name;
+    this.checkName();
+
     this.type = type;
+    this.checkType();
+
     this.health = 100;
     this.level = 1;
-    this.attack = 0;
-    this.defence = 0;
-    this.checkName();
-    this.checkType();
+    this.attack = undefined;
+    this.defence = undefined;
   }
 
   checkName() {
@@ -24,22 +26,20 @@ export default class Character {
   }
 
   levelUp() {
-    if (this.health > 0) {
-      this.level += 1;
-      this.attack *= 1.2;
-      this.defence *= 1.2;
-      this.health = 100;
-      return;
+    if (this.health === 0) {
+      throw new Error('Cannot levelUp. You are Gameover');
     }
 
-    throw new Error('Cannot levelUp. You are Gameover');
+    this.level += 1;
+    this.attack *= 1.2;
+    this.defence *= 1.2;
+    this.health = 100;
   }
 
   damage(points) {
-    if (this.health > 0) {
-      this.health -= points * (1 - this.defence / 100);
-      return;
+    if (this.health <= 0) {
+      throw new Error('Gameover!');
     }
-    throw new Error('Gameover!');
+    this.health -= points * (1 - this.defence / 100);
   }
 }
